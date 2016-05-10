415f65583c97de12d55cba316aa721bdb887c665
zhouyq <zyq916@gmail.com>
2016-05-10 22:52:51 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[ $DEBUG ] && set -x
+
+
+set -e
+
+export ME_CONFIG_EDITORTHEME="default"
+export ME_CONFIG_MONGODB_SERVER=${MONGO_HOST:-127.0.0.1}
+export ME_CONFIG_MONGODB_PORT=${MONGO_PORT:-27017}
+export ME_CONFIG_MONGODB_ENABLE_ADMIN="true"
+export ME_CONFIG_BASICAUTH_USERNAME=${ME_USER:-""}
+export ME_CONFIG_BASICAUTH_PASSWORD=${ME_PASS:-""}
+export VCAP_APP_HOST="0.0.0.0"
+
+exec "$@"
